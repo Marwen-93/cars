e7732a8b535d378ba4c1300e02289e317995b705
Marwen-93 <marwenweslati@yahoo.com>
2021-11-15 22:56:48 +0100

    home update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 
 const Home = () => {
-    return(
-        <div>
-            <h1> anvgation lis</h1>
-            <p>emp</p>
-            <p>addemp</p>
-           
-            
+    return (
+        <div >
+                    <div className="container">
+
+            <h1> About</h1>
+            <h3>These simple CRUD applications with Django and Django rest framework and react js</h3>
+            <h2>Marwen Weslati</h2>
+            <h3> Python and Django Full Stack Developer </h3>
+
+        
+            <div class="card">
+                <div class="card-header">
+                    Contact me
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><h2>54 665 680 </h2></li>
+                    <li class="list-group-item"><h2>  marwenweslati@yahoo.com </h2></li>
+
+                    <li class="list-group-item"><h2><a href="http://marwenweslati.herokuapp.com/" className="fab fa-internet-explorer card-text" target="_blank">  <p>      Portfilio</p>   </a></h2>
+ </li>
+                    <li class="list-group-item"><h2><a href="https://www.linkedin.com/in/marwen-weslati/" className="fa fa-linkedin" target="_blank">  <p>    Linkedin</p></a></h2>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
         </div>
     );
 
